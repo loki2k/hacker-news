d85e0cf9f38d01b576aaa1ae62ce8d2a939dab4c
loki2k <jeremylagarto@gmail.com>
2020-12-13 11:34:06 -0800

    Updates

---
+++ precache-manifest.32d1a1e34a9805213df0464b7420cdc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbff307c4811a524ccbe1de8ed0683c",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "81865139ed4599e6b286",
+    "url": "/hacker-news/static/css/main.35784568.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/hacker-news/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81865139ed4599e6b286",
+    "url": "/hacker-news/static/js/main.63cfb9b1.chunk.js"
+  },
+  {
+    "revision": "1673c2e233d4c15aeda0",
+    "url": "/hacker-news/static/js/runtime-main.2c7535f1.js"
+  }
+]);
